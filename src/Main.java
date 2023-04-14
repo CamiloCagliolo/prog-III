d616a66d208994eb072a6df48c53da592fafc9d7
Camilo Cagliolo <camilocagliolo14@gmail.com>
2023-04-13 22:57:26 -0300

    Terminado el práctico 1

--- src/Main.java
+++ src/Main.java
@@ -40,16 +40,22 @@ public class Main {
         System.out.println(doublelist);
         System.out.println(doublelist.get(6));
 
-        System.out.println(isPalindrome("abcba"));*/
+        System.out.println(isPalindrome("abcba"));
 
         Integer[] arr = new Integer[10];
         for(int i = 0; i < arr.length; i++){
-            arr[i] = i+1;
+            arr[i] = 2*i+1;
         }
 
-        System.out.println(arrayIsOrdered(arr));
+        System.out.println(getIndexOf(arr, 6));*/
+
+
+        System.out.println(convertToBinary(16));
+
+        fibonacci(8);
     }
 
+    //Ejercicio 9
     public static boolean isPalindrome(String s){
         if(s.length() == 1 || s.length() == 0){
             return true;
@@ -62,6 +68,12 @@ public class Main {
             return false;
         }
     }
+
+    //Ejercicio 10
+    //¿Qué complejidad O tiene? (La complejidad en el peor caso): Tiene complejidad O(n). Si está efectivamente ordenado, tendría que llamar a la función n veces.
+    //¿Trae algún problema hacerlo recursivo? ¿Cuál?: Se podrían agregar demasiadas llamadas a la función recursiva que sobrepasen la capacidad de la pila (no estoy seguro)
+    //¿Qué cambiaría si la estructura fuera una lista en lugar de un arreglo?: No se podría acceder directamente al elemento en una determinada posición
+    // sin tener que pasar por los elementos anteriores. El orden de complejidad es O(n^2).
 
     public static <S extends Comparable<S>> boolean arrayIsOrdered(S[] arr){
         return check(0, arr);
@@ -76,6 +88,44 @@ public class Main {
         }
         else{
             return false;
+        }
+    }
+
+    //Ejercicio 11
+    public static <S extends Comparable<S>> int getIndexOf(S[] arr, S element){
+        return getIndexOf(0, arr, element);
+    }
+
+    public static <S extends Comparable<S>> int getIndexOf(int index, S[] arr, S element){
+        if(index == arr.length || element.compareTo(arr[index]) < 0){
+            return -1;
+        }
+        if(element.compareTo(arr[index]) > 0){
+            index++;
+            return getIndexOf(index, arr, element);
+        }
+        return index;
+    }
+
+    //Ejercicio 12
+    public static String convertToBinary(int n){
+        if(n == 0){
+            return "";
+        }
+        int remainder = n%2;
+        int quotient = (n-remainder)/2;
+        return convertToBinary(quotient) + remainder;
+    }
+
+    //Ejercicio 13
+    public static void fibonacci(int n){
+        fibonacci(0,1,0, n);
+    }
+
+    public static void fibonacci(int am, int an, int index, int n){
+        System.out.println(am);
+        if(index <= n){
+            fibonacci(an, an+am, index+1, n);
         }
     }
 }
